c1eaf73baefaed09c5e228b7067f6f6916fb316e
James <jameseqbonner@protonmail.com>
2021-11-29 18:01:29 -0700

    switched over to .NET 6

--- Website.Backend/WeatherForecast.cs
+++ Website.Backend/WeatherForecast.cs
@@ -1,6 +1,4 @@
-using System;
-
-namespace WebApi
+namespace Website.Backend
 {
     public class WeatherForecast
     {
@@ -10,6 +8,6 @@ namespace WebApi
 
         public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 
-        public string Summary { get; set; }
+        public string? Summary { get; set; }
     }
 }

289a87ba4fccf0c62333a6f0dce0ac351f257cd8
James <jameseqbonner@protonmail.com>
2022-04-23 23:49:37 -0700

    added FinancialInformation placeholders

---
+++ Website.Backend/Controllers/FinancialInformationController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Website.Backend.Models;
+
+namespace Website.Backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FinancialInformationController : ControllerBase
+    {
+        public FinancialInformationController()
+        {
+
+        }
+
+        // GET: api/<FinancialInformationController>
+        [HttpGet]
+        [ProducesResponseType(typeof(FinancialInformationModel), 200)]
+        public async Task<IActionResult> Get()
+        {
+            // TODO: due to api rate limiting, probably need to make a background
+            // process that updates a table every N minutes. Then there is a service
+            // that reads the LATEST results from that/those tables.
+            FinancialInformationModel info = new();
+
+            return Ok(info);
+        }
+    }
+}
---
+++ Website.Backend/Models/FinancialInformationModel.cs
@@ -0,0 +1,10 @@
+﻿namespace Website.Backend.Models
+{
+    public class FinancialInformationModel
+    {
+        // Price per ounce
+        public decimal GoldSpotPrice { get; set; }
+
+        public decimal BitcoinPrice { get; set; }
+    }
+}

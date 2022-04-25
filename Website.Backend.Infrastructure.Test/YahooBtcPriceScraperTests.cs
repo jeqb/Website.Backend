fa6ecf698b4f88609ca303f5a1bb9cb701cf1b5b
James <jameseqbonner@protonmail.com>
2022-04-24 20:14:02 -0700

    YahooBtcPriceScraper scrapes yahoo for BTC price

---
+++ Website.Backend.Infrastructure.Test/YahooBtcPriceScraperTests.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Extensions.Logging.Abstractions;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Website.Backend.Infrastructure.Test
+{
+    public class YahooBtcPriceScraperTests
+    {
+        private readonly NullLogger<YahooBtcPriceScraper> _nullLogger;
+
+        private readonly HttpClient _httpClient;
+
+        public YahooBtcPriceScraperTests()
+        {
+            _nullLogger = new();
+
+            _httpClient = new();
+        }
+
+        [Fact]
+        public async Task GetBtcPriceInUsd_WhenCalled_ReturnsNonZero()
+        {
+            // Arrange
+
+            YahooBtcPriceScraper target = new(_nullLogger, _httpClient);
+
+
+            // Act
+
+            decimal result = await target.GetBtcPriceInUsd();
+
+
+            // Assert
+
+            Assert.True(result > 0);
+        }
+    }
+}
---
+++ Website.Backend.Infrastructure/YahooBtcPriceScraper.cs
@@ -0,0 +1,70 @@
+﻿using HtmlAgilityPack;
+using Microsoft.Extensions.Logging;
+using Website.Backend.Infrastructure.Interfaces;
+
+namespace Website.Backend.Infrastructure
+{
+    public class YahooBtcPriceScraper : ICryptoCurrencyService
+    {
+        private const string _url = "https://finance.yahoo.com/quote/BTC-USD/";
+
+        private readonly ILogger<YahooBtcPriceScraper> _logger;
+
+        private readonly HttpClient _httpClient;
+
+        public YahooBtcPriceScraper(ILogger<YahooBtcPriceScraper> logger, HttpClient httpClient)
+        {
+            _logger = logger;
+
+            _httpClient = httpClient;
+        }
+
+        public async Task<decimal> GetBtcPriceInUsd()
+        {
+            HttpResponseMessage response = await _httpClient.GetAsync(_url);
+
+            if (response.IsSuccessStatusCode != true)
+            {
+                _logger.LogInformation("YahooBtcPriceScraper could not retrieve BTC price.");
+
+                return 0;
+            }
+            else
+            {
+                string responseContent = await response.Content.ReadAsStringAsync();
+
+                decimal price = ParseHtml(responseContent);
+
+                return price;
+            }
+        }
+
+        private decimal ParseHtml(string html)
+        {
+            HtmlDocument htmlDoc = new HtmlDocument();
+            htmlDoc.LoadHtml(html);
+
+            // https://stackoverflow.com/questions/36711680/c-sharp-html-agility-pack-get-elements-by-class-name
+            HtmlNodeCollection nodeCollection = htmlDoc.DocumentNode.SelectNodes(
+                "//fin-streamer[contains(@class, 'Fw(b) Fz(36px) Mb(-4px) D(ib)')]"
+                );
+
+            HtmlNode? finalNode = nodeCollection.FirstOrDefault();
+
+            if (finalNode == null)
+            {
+                _logger.LogInformation("YahooBtcPriceScraper could not find final HTML element with price");
+
+                return 0;
+            }
+            else
+            {
+                string stringPrice = finalNode.InnerText;
+
+                decimal price = Convert.ToDecimal(stringPrice);
+
+                return price;
+            }
+        }
+    }
+}
